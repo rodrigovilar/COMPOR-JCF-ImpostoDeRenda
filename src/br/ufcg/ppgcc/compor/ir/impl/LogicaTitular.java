c860072685d5c29df9be42024c0edca6f9425a8c
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2013-08-16 19:10:53 -0300

    T_01_04 passando

--- src/br/ufcg/ppgcc/compor/ir/impl/LogicaTitular.java
+++ src/br/ufcg/ppgcc/compor/ir/impl/LogicaTitular.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 import br.ufcg.ppgcc.compor.ir.fachada.Titular;
 
-
 public class LogicaTitular {
-	
+
 	private List<Titular> titulares = new ArrayList<Titular>();
 
 	public void criarNovoTitular(Titular titular) {
+		Validacao.obrigatorio(titular.getNome(),
+				"O campo nome é obrigatório para o titular");
 		titulares.add(titular);
 	}
 
