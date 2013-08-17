e22c66286f983505035bf733581ac1d62cdb373f
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2013-08-17 13:24:17 -0300

    T_02_01

---
+++ src/br/ufcg/ppgcc/compor/ir/teste/FontePagadoraTest.java
@@ -0,0 +1,28 @@
+package br.ufcg.ppgcc.compor.ir.teste;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ufcg.ppgcc.compor.ir.fachada.FachadaExperimento;
+import br.ufcg.ppgcc.compor.ir.fachada.FontePagadora;
+import br.ufcg.ppgcc.compor.ir.fachada.Titular;
+
+public class FontePagadoraTest {
+
+	private FachadaExperimento fachada;
+
+	@Before
+	public void iniciar() {
+		//Coloque sua Fachada aqui.
+		fachada = null;
+	}
+
+	@Test
+	public void T_02_01_novaFontePagadora() {
+		Titular titular = TitularHelper.criarTitularPadrao();
+		fachada.criarNovoTitular(titular);
+		FontePagadora fonte = FontePagadoraHelper.criarFontePagadoraPadrao1();
+		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular, fonte);
+	}
+
+}
