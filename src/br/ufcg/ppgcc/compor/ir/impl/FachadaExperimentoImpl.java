e70f2244873b34a711b847e14d74b37b57682cd9
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2013-08-18 11:21:23 -0300

    T_02_01 passando

--- src/br/ufcg/ppgcc/compor/ir/impl/FachadaExperimentoImpl.java
+++ src/br/ufcg/ppgcc/compor/ir/impl/FachadaExperimentoImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import net.compor.frameworks.jcf.api.ComporFacade;
 import br.ufcg.ppgcc.compor.ir.fachada.FachadaExperimento;
+import br.ufcg.ppgcc.compor.ir.fachada.FontePagadora;
 import br.ufcg.ppgcc.compor.ir.fachada.Titular;
 
 public class FachadaExperimentoImpl extends ComporFacade implements
@@ -14,6 +15,8 @@ public class FachadaExperimentoImpl extends ComporFacade implements
 		GerenteTitular gerenteTitular = new GerenteTitular();
 		add(gerenteTitular);
 		add(new ValidacaoTitular(gerenteTitular));
+		
+		add(new GerenteFontePagadora());
 	}
 
 	public void criarNovoTitular(Titular titular) {
@@ -24,4 +27,15 @@ public class FachadaExperimentoImpl extends ComporFacade implements
 	public List<Titular> listarTitulares() {
 		return (List<Titular>) requestService("listarTitulares");
 	}
+	
+	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
+		requestService("criarFontePagadora", titular, fonte);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<FontePagadora> listarFontes(Titular titular) {
+		return (List<FontePagadora>) requestService("listarFontesPagadoras",
+				titular);
+	}
+
 }
---
+++ src/br/ufcg/ppgcc/compor/ir/impl/GerenteFontePagadora.java
@@ -0,0 +1,40 @@
+package br.ufcg.ppgcc.compor.ir.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.compor.frameworks.jcf.api.Component;
+import net.compor.frameworks.jcf.api.Service;
+import br.ufcg.ppgcc.compor.ir.fachada.FontePagadora;
+import br.ufcg.ppgcc.compor.ir.fachada.Titular;
+
+public class GerenteFontePagadora extends Component {
+
+	private Map<Titular, List<FontePagadora>> fontesPagadoras = 
+			new HashMap<Titular, List<FontePagadora>>();
+
+	public GerenteFontePagadora() {
+		super("Gerente de fontes pagadoras");
+	}
+
+	@Service
+	public void criarFontePagadora(Titular titular,
+			FontePagadora fontePagadora) {
+		inicializarLista(titular);
+		fontesPagadoras.get(titular).add(fontePagadora);
+	}
+
+	@Service
+	public List<FontePagadora> listarFontesPagadoras(Titular titular) {
+		inicializarLista(titular);
+		return fontesPagadoras.get(titular);
+	}
+	
+	private void inicializarLista(Titular titular) {
+		if (fontesPagadoras.get(titular) == null) {
+			fontesPagadoras.put(titular, new ArrayList<FontePagadora>());
+		}
+	}
+}
