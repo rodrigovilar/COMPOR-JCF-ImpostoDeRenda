02d599bec019be16994d43df99a4764cf401dedb
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2013-08-19 16:15:36 -0300

    T_02_06 passando

--- src/br/ufcg/ppgcc/compor/ir/impl/ValidacaoFontePagadora.java
+++ src/br/ufcg/ppgcc/compor/ir/impl/ValidacaoFontePagadora.java
@@ -1,7 +1,10 @@
 package br.ufcg.ppgcc.compor.ir.impl;
 
+import java.util.List;
+
 import net.compor.frameworks.jcf.api.Decorator;
 import net.compor.frameworks.jcf.api.Service;
+import br.ufcg.ppgcc.compor.ir.fachada.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.fachada.FontePagadora;
 import br.ufcg.ppgcc.compor.ir.fachada.Titular;
 
@@ -11,7 +14,7 @@ public class ValidacaoFontePagadora extends Decorator<GerenteFontePagadora> {
 		super(innerComponent);
 	}
 
-	@Service
+	@Service(requiredServices="listarTitulares")
 	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
 		Validacao.obrigatorio(fonte.getNome(), "O campo nome é obrigatório");
 		Validacao.obrigatorio(fonte.getCpfCnpj(),
@@ -21,6 +24,16 @@ public class ValidacaoFontePagadora extends Decorator<GerenteFontePagadora> {
 		Validacao.numeroMaiorQueZero(fonte.getRendimentosRecebidos(),
 				"O campo rendimentos recebidos deve ser maior que zero");
 		Validacao.cpfOuCnpj(fonte.getCpfCnpj(), "O campo CPF/CNPJ é inválido");
+		validar(titular);
+
 		getInnerComponent().criarFontePagadora(titular, fonte);
+	}
+
+	@SuppressWarnings("unchecked")
+	private void validar(Titular titular) {
+		List<Titular> titulares = (List<Titular>) requestService("listarTitulares");
+		if (!titulares.contains(titular)) {
+			throw new ExcecaoImpostoDeRenda("Titular não cadastrado");
+		}
 	}
 }
