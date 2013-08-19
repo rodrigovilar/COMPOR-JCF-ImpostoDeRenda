406b34b84e04b5902f75ad0944e41115679c3a80
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2013-08-19 19:08:03 -0300

    T_03_04 T_03_05 T_03_06

--- src/br/ufcg/ppgcc/compor/ir/teste/DependenteTest.java
+++ src/br/ufcg/ppgcc/compor/ir/teste/DependenteTest.java
@@ -1,5 +1,7 @@
 package br.ufcg.ppgcc.compor.ir.teste;
 
+import java.util.Calendar;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,7 +25,8 @@ public class DependenteTest {
 		fachada.criarNovoTitular(titular);
 
 		Dependente dependente = DependenteHelper.criarDependentePadrao1();
-		DependenteHelper.verificaCriacaoDependentes(fachada, titular, dependente);
+		DependenteHelper.verificaCriacaoDependentes(fachada, titular,
+				dependente);
 	}
 
 	@Test
@@ -32,7 +35,8 @@ public class DependenteTest {
 		fachada.criarNovoTitular(titular);
 		Dependente dependente1 = DependenteHelper.criarDependentePadrao1();
 		Dependente dependente2 = DependenteHelper.criarDependentePadrao2();
-		DependenteHelper.verificaCriacaoDependentes(fachada, titular, dependente1, dependente2);
+		DependenteHelper.verificaCriacaoDependentes(fachada, titular,
+				dependente1, dependente2);
 	}
 
 	@Test
@@ -45,8 +49,66 @@ public class DependenteTest {
 		fachada.criarNovoTitular(titular2);
 		Dependente dependente2 = DependenteHelper.criarDependentePadrao2();
 
-		DependenteHelper.verificaCriacaoDependentes(fachada, titular1, dependente1);
-		DependenteHelper.verificaCriacaoDependentes(fachada, titular2, dependente2);
+		DependenteHelper.verificaCriacaoDependentes(fachada, titular1,
+				dependente1);
+		DependenteHelper.verificaCriacaoDependentes(fachada, titular2,
+				dependente2);
+	}
+
+	@Test
+	public void T_03_04_validacaoDependente() {
+		Titular titular = TitularHelper.criarTitularPadrao();
+		fachada.criarNovoTitular(titular);
+
+		Dependente dependenteSemCpf = DependenteHelper.criarDependente(null,
+				Calendar.getInstance(), "Filho 1", 21);
+		DependenteHelper.excecaoCriarDependente(fachada, titular,
+				dependenteSemCpf, "O campo CPF é obrigatório");
+
+		Dependente dependenteSemNome = DependenteHelper.criarDependente(
+				"000.000.000-00", Calendar.getInstance(), null, 21);
+		DependenteHelper.excecaoCriarDependente(fachada, titular,
+				dependenteSemNome, "O campo nome é obrigatório");
+
+		Dependente dependenteSemTipo = DependenteHelper.criarDependente(
+				"000.000.000-00", Calendar.getInstance(), "Filho", 0);
+		DependenteHelper.excecaoCriarDependente(fachada, titular,
+				dependenteSemTipo, "O campo tipo é obrigatório");
+
+		Dependente dependenteComTipoInvalido = DependenteHelper
+				.criarDependente("000.000.000-00", Calendar.getInstance(),
+						"Filho", -10);
+		DependenteHelper.excecaoCriarDependente(fachada, titular,
+				dependenteComTipoInvalido, "O campo tipo é inválido");
+	}
+	
+	@Test
+	public void T_03_05_novoDependenteComCpfInvalido() {
+		Titular titular = TitularHelper.criarTitularPadrao();
+		fachada.criarNovoTitular(titular);
+
+		Dependente dependente = 
+				DependenteHelper.criarDependente("000000000000", Calendar.getInstance(), "Filho 1", 21);
+		DependenteHelper.excecaoCriarDependente(fachada, titular,
+				dependente, "O campo CPF é inválido");
+		
+		dependente = 
+				DependenteHelper.criarDependente("abc", Calendar.getInstance(), "Filho 1", 21);
+		DependenteHelper.excecaoCriarDependente(fachada, titular,
+				dependente, "O campo CPF é inválido");
+
+		dependente = 
+				DependenteHelper.criarDependente("000.000.000.00a", Calendar.getInstance(), "Filho 1", 21);
+		DependenteHelper.excecaoCriarDependente(fachada, titular,
+				dependente, "O campo CPF é inválido");
+	}
+
+	@Test
+	public void T_03_06_novoDependenteComTitularDesconhecido() {
+		Titular titular = TitularHelper.criarTitularPadrao();
+		Dependente dependente = DependenteHelper.criarDependentePadrao1();
+		DependenteHelper.excecaoCriarDependente(fachada, titular, dependente,
+				"Titular não cadastrado");
 	}
 
 }
