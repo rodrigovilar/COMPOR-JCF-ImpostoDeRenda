90f5442b0548466e6206474dbdaa1fb2fbd173a7
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2013-08-19 11:32:43 -0300

    T_02_04

--- src/br/ufcg/ppgcc/compor/ir/teste/FontePagadoraTest.java
+++ src/br/ufcg/ppgcc/compor/ir/teste/FontePagadoraTest.java
@@ -1,9 +1,5 @@
 package br.ufcg.ppgcc.compor.ir.teste;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,7 +13,7 @@ public class FontePagadoraTest {
 
 	@Before
 	public void iniciar() {
-		//Coloque sua Fachada aqui.
+		// Coloque sua Fachada aqui.
 		fachada = null;
 	}
 
@@ -35,9 +31,10 @@ public class FontePagadoraTest {
 		fachada.criarNovoTitular(titular);
 		FontePagadora fonte1 = FontePagadoraHelper.criarFontePagadoraPadrao1();
 		FontePagadora fonte2 = FontePagadoraHelper.criarFontePagadoraPadrao2();
-		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular, fonte1, fonte2);
+		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular, fonte1,
+				fonte2);
 	}
-	
+
 	@Test
 	public void T_02_03_duasFontesPagadorasUmaEmCadaTitular() {
 		Titular titular1 = TitularHelper.criarTitularPadrao();
@@ -49,5 +46,28 @@ public class FontePagadoraTest {
 		fachada.criarNovoTitular(titular2);
 		FontePagadora fonte2 = FontePagadoraHelper.criarFontePagadoraPadrao2();
 		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular2, fonte2);
+	}
+
+	@Test
+	public void T_02_04_validacaoFontePagadora() {
+		Titular titular = TitularHelper.criarTitularPadrao();
+		FontePagadora fonteSemNome = FontePagadoraHelper.criarFontePagadora(
+				null, "000.000.000/0000-00", 50000);
+		FontePagadoraHelper.excecaoCriarFonte(fachada, titular, fonteSemNome,
+				"O campo nome é obrigatório");
+
+		FontePagadora fonteSemCPFCNPJ = FontePagadoraHelper.criarFontePagadora("UFCG", null, 50000);
+		FontePagadoraHelper.excecaoCriarFonte(fachada, titular, fonteSemCPFCNPJ,
+				"O campo CPF/CNPJ é obrigatório");
+
+		FontePagadora fonteSemRendimentos = FontePagadoraHelper.criarFontePagadora("UFCG", "000.000.000/0000-00",
+				0);
+		FontePagadoraHelper.excecaoCriarFonte(fachada, titular, fonteSemRendimentos,
+				"O campo rendimentos recebidos é obrigatório");
+
+		FontePagadora fonteComRendimentosNegativos = FontePagadoraHelper.criarFontePagadora("UFCG", "00.000.000/0000-00",
+				-1000);
+		FontePagadoraHelper.excecaoCriarFonte(fachada, titular, fonteComRendimentosNegativos,
+				"O campo rendimentos recebidos deve ser maior que zero");
 	}
 }
