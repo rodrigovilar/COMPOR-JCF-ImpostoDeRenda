4a3f12824b8310635e76afdcf9a53e4d2eecd0bc
Rodrigo Vilar <rodrigovilar@dce.ufpb.br>
2013-12-16 16:35:38 -0300

    T_06_06 T_06_07 T_06_08 T_06_09

--- src/br/ufcg/ppgcc/compor/ir/teste/AutenticacaoTest.java
+++ src/br/ufcg/ppgcc/compor/ir/teste/AutenticacaoTest.java
@@ -1,10 +1,15 @@
 package br.ufcg.ppgcc.compor.ir.teste;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import br.ufcg.ppgcc.compor.ir.fachada.Dependente;
 import br.ufcg.ppgcc.compor.ir.fachada.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.fachada.FachadaExperimento;
+import br.ufcg.ppgcc.compor.ir.fachada.FontePagadora;
+import br.ufcg.ppgcc.compor.ir.fachada.Resultado;
+import br.ufcg.ppgcc.compor.ir.fachada.Titular;
 
 public class AutenticacaoTest {
 
@@ -41,4 +46,54 @@ public class AutenticacaoTest {
 		fachada.criarUsuario("usuario", "senha");
 	}
 
+	@Test
+	public void T_06_06_criarTitular() {
+		Titular titular1 = TitularHelper.criarTitularPadrao();
+		TitularHelper.excecaoCriarTitular(fachada, titular1, "Usuário não logado");
+		
+		AutenticacaoHelper.adminCriarUsuarioLogar(fachada, "usuario", "senha");
+		TitularHelper.verificaCriacaoTitulares(fachada, titular1);
+	}
+
+	@Test
+	public void T_06_07_criarFontePagadora() {
+		Titular titular1 = TitularHelper.criarTitularPadrao();
+		FontePagadora fonte1 = FontePagadoraHelper.criarFontePagadoraPadrao1();
+		FontePagadoraHelper.excecaoCriarFonte(fachada, titular1, fonte1, "Usuário não logado");
+		
+		AutenticacaoHelper.adminCriarUsuarioLogar(fachada, "usuario", "senha");
+		TitularHelper.verificaCriacaoTitulares(fachada, titular1);
+		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular1, fonte1);
+	}
+
+	@Test
+	public void T_06_08_criarDependente() {
+		Titular titular1 = TitularHelper.criarTitularPadrao();
+		Dependente dependente1 = DependenteHelper.criarDependentePadrao1();
+		DependenteHelper.excecaoCriarDependente(fachada, titular1, dependente1, "Usuário não logado");
+		
+		AutenticacaoHelper.adminCriarUsuarioLogar(fachada, "usuario", "senha");
+		TitularHelper.verificaCriacaoTitulares(fachada, titular1);
+		DependenteHelper.verificaCriacaoDependentes(fachada, titular1, dependente1);
+	}
+
+	@Test
+	public void T_06_09_declaracaoCompleta() {
+		Titular titular1 = TitularHelper.criarTitularPadrao();
+
+		try {
+			fachada.declaracaoCompleta(titular1);
+			Assert.fail("A declaração completa deveria ter lançado exceção");
+		} catch (ExcecaoImpostoDeRenda e) {
+			Assert.assertEquals(e.getMessage(), "Usuário não logado");
+		}
+
+		AutenticacaoHelper.adminCriarUsuarioLogar(fachada, "usuario", "senha");
+		
+		TitularHelper.verificaCriacaoTitulares(fachada, titular1);
+		FontePagadora fonte1 = FontePagadoraHelper.criarFontePagadoraPadrao1();
+		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular1, fonte1);
+		Resultado completa = fachada.declaracaoCompleta(titular1);
+		Assert.assertNotNull(completa);
+	}
 }
