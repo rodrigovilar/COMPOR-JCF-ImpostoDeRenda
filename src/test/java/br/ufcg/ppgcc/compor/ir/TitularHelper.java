5de71c13e4f37b1bbaaa9bd1e32b794e5c67d1f1
rodrigovilar <rvilar@gmail.com>
2014-08-23 06:39:12 -0300

    Teste 01.02

--- src/test/java/br/ufcg/ppgcc/compor/ir/TitularHelper.java
+++ src/test/java/br/ufcg/ppgcc/compor/ir/TitularHelper.java
@@ -2,6 +2,7 @@ package br.ufcg.ppgcc.compor.ir;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class TitularHelper {
@@ -11,6 +12,28 @@ public class TitularHelper {
 		titular.setNome("Jose");
 		titular.setCpf("000.000.000-00");
 		return titular;
+	}
+	
+	static Titular criarTitularPadrao() {
+		Titular titular = criarTitularMinimo();
+		titular.setDataNascimento(Calendar.getInstance());
+		titular.setTituloEleitoral("000");
+		titular.setEndereco(criarEnderecoPadrao());
+		titular.setNaturezaOcupacao(10);
+		titular.setOcupacaoPrincipal(100);
+		return titular;
+	}
+
+	static Endereco criarEnderecoPadrao() {
+		Endereco endereco = new Endereco();
+		endereco.setLogradouro("Log");
+		endereco.setNumero(0);
+		endereco.setComplemento("A");
+		endereco.setBairro("Bai");
+		endereco.setMunicipio("Mun");
+		endereco.setEstado("Est");
+		endereco.setCEP("00000-000");
+		return endereco;
 	}
 
 	static void verificaCriacaoTitular(FachadaExperimento fachada, Titular titular) {
--- src/test/java/br/ufcg/ppgcc/compor/ir/TitularTest.java
+++ src/test/java/br/ufcg/ppgcc/compor/ir/TitularTest.java
@@ -3,7 +3,6 @@ package br.ufcg.ppgcc.compor.ir;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class TitularTest {
 
 	private FachadaExperimento fachada;
@@ -16,6 +15,12 @@ public class TitularTest {
 	@Test
 	public void T_01_01_novoTitular() {
 		Titular titular = TitularHelper.criarTitularMinimo();
+		TitularHelper.verificaCriacaoTitular(fachada, titular);
+	}
+	
+	@Test
+	public void T_01_02_titularCompleto() {
+		Titular titular = TitularHelper.criarTitularPadrao();
 		TitularHelper.verificaCriacaoTitular(fachada, titular);
 	}
 
