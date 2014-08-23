c4ccacc14d2adf3db2d767f14f89c9553f412df9
rodrigovilar <rvilar@gmail.com>
2014-08-23 07:27:26 -0300

    Teste 02.03

--- src/test/java/br/ufcg/ppgcc/compor/ir/FontePagadoraTest.java
+++ src/test/java/br/ufcg/ppgcc/compor/ir/FontePagadoraTest.java
@@ -29,5 +29,18 @@ public class FontePagadoraTest {
 		FontePagadora fonte2 = FontePagadoraHelper.criarFontePagadoraPadrao2();
 		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular, fonte1, fonte2);
 	}
+	
+	@Test
+	public void T_02_03_duasFontesPagadorasUmaEmCadaTitular() {
+		Titular titular1 = TitularHelper.criarTitularPadrao();
+		fachada.criarNovoTitular(titular1);
+		FontePagadora fonte1 = FontePagadoraHelper.criarFontePagadoraPadrao1();
+		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular1, fonte1);
+
+		Titular titular2 = TitularHelper.criarTitularMinimo();
+		fachada.criarNovoTitular(titular2);
+		FontePagadora fonte2 = FontePagadoraHelper.criarFontePagadoraPadrao2();
+		FontePagadoraHelper.verificaCriacaoFontes(fachada, titular2, fonte2);
+	}
 
 }
